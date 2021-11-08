c9074f5dfcbb5cd97fe2d6e24e85272494cc027b
Gagandeep Uppal <guppal.profile@gmail.com>
2021-11-08 21:52:20 +0000

    update newName for store 1

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,13 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+##Load the first store (with id = 1) from the database and assign it to an instance variable @store1.
+@store1 = Store.find_by(id: 1)
+puts @store1.name
+
+##Load the second store from the database and assign it to @store2.
+@store2 = Store.find_by(id: 2)
+puts @store2.name
+
+##Update the first store (@store1) instance in the database. (Change its name or something.)
+ @store1.update(name: "New_Name")
