71bb062b77ffd7b1efcd7ad59999fb31ae0819c2
Gagandeep Uppal <guppal.profile@gmail.com>
2021-11-08 22:52:40 +0000

    employees added for store1 and store2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Dave", hourly_rate: 40)
+@store1.employees.create(first_name: "Bella", last_name: "Smith", hourly_rate: 30)
+
+@store2.employees.create(first_name: "Gagan", last_name: "Uppal", hourly_rate: 70)
+@store2.employees.create(first_name: "William", last_name: "Jones", hourly_rate: 60)
+@store2.employees.create(first_name: "David", last_name: "Drummer", hourly_rate: 50)
+
